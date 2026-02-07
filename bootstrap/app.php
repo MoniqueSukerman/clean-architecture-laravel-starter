3f776e6f0689574e964785bfe337987eadfbf797
Monique Sukerman <sukermanmsp@agilize.com.br>
2026-02-07 16:07:56 -0300

    Remove logs de debug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Domain\Exception\Http\HttpException;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
